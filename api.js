f542df1b023eb725482eb521a3389c66ab2ddded
adrianl27 <alopezv188@ulacit.ed.cr>
2023-12-14 16:55:46 -0600

    .

--- api.js
+++ api.js
@@ -1,3 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Asociar la función crearUsuario al evento click del botón
+    document.getElementById("btnCrearUsuario").addEventListener("click", crearUsuario);
+
 function crearUsuario() {
     // Obtener los valores del formulario
     const usuario = document.getElementById('InputUser').value;
@@ -18,7 +22,7 @@ function crearUsuario() {
     };
 
     // Realizar la solicitud POST
-    fetch('URL_DE_TU_API', {
+    fetch('https://localhost:7246/api/Usuario', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -34,3 +38,39 @@ function crearUsuario() {
         console.error('Error:', error);
     });
 }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Manejar el evento click del botón "Cambiar Contraseña"
+    document.getElementById("btnCambiarContrasena").addEventListener("click", function () {
+        // Obtener los valores de los campos de contraseña
+        var contrasenaActual = document.getElementById("InputPassAct").value;
+        var nuevaContrasena = document.getElementById("InputPasswNew").value;
+        var confirmarContrasena = document.getElementById("InputPasswNewConf").value;
+
+        // Construir el objeto de datos
+        var data = {
+            contrasenaActual: contrasenaActual,
+            nuevaContrasena: nuevaContrasena,
+            confirmarContrasena: confirmarContrasena
+        };
+
+        // Hacer la solicitud Fetch
+        fetch("https://localhost:7246/api/Usuario", {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+        .then(response => response.json())
+        .then(data => {
+            // Manejar la respuesta exitosa del servidor
+            console.log(data);
+        })
+        .catch(error => {
+            // Manejar errores
+            console.error("Error:", error);
+        });
+    });
+});
